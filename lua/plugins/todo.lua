29f80a86396dd980ba5f49bd0e1a14c8ab6c0325
Diego Velez <diego.velez.dev@gmail.com>
2025-09-23 15:09:19 -0400

    feat: Replace nerd font icons from todo-comments with ASCII

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -1,6 +1,29 @@
 local todo = require 'todo-comments'
 todo.setup {
   signs = true,
+  keywords = {
+    FIX = {
+      icon = 'F ',
+    },
+    TODO = {
+      icon = 'T ',
+    },
+    HACK = {
+      icon = 'H ',
+    },
+    WARN = {
+      icon = 'W ',
+    },
+    PERF = {
+      icon = 'P ',
+    },
+    NOTE = {
+      icon = 'N ',
+    },
+    TEST = {
+      icon = 'T ',
+    },
+  },
 }
 
 vim.keymap.set('n', '[t', todo.jump_prev, { desc = 'Previous [t]odo comment' })
